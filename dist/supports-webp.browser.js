f7552b830304672b2516aca340163771839933a6
Federico Brigante <bfred-it@users.noreply.github.com>
2017-02-01 18:33:56 -0800

    Updated built file

--- dist/supports-webp.browser.js
+++ dist/supports-webp.browser.js
@@ -1,2 +1,2 @@
 /*! npm.im/supports-webp */
-var supportsWebP=function(){"use strict";var t=document.createElement("canvas");t.width=t.height=1;var a=t.toDataURL&&0===t.toDataURL("image/webp").indexOf("data:image/webp");return a}();
+var supportsWebP=function(){"use strict";var t=document.createElement("canvas");t.width=t.height=1;return t.toDataURL&&5===t.toDataURL("image/webp").indexOf("image/webp")}();
