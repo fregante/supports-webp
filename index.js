f7705dabcf28ac6aba4555952c35a8ebaa6acde0
Federico Brigante <bfred-it@users.noreply.github.com>
2017-05-09 10:04:12 +0800

    Add support for server-side rendering
    Fixes #3

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const canvas = document.createElement('canvas');
+const canvas = typeof document === 'object' ? document.createElement('canvas') : {};
 canvas.width = canvas.height = 1;
 export default canvas.toDataURL && canvas.toDataURL('image/webp').indexOf('image/webp') === 5;
