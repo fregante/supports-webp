52014b011cc88e002f002ac3fa0c7e9daa76859b
Federico Brigante <bfred-it@users.noreply.github.com>
2017-05-09 10:04:37 +0800

    Always return a boolean even if completely unsupported

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const canvas = typeof document === 'object' ? document.createElement('canvas') : {};
 canvas.width = canvas.height = 1;
-export default canvas.toDataURL && canvas.toDataURL('image/webp').indexOf('image/webp') === 5;
+export default canvas.toDataURL ? canvas.toDataURL('image/webp').indexOf('image/webp') === 5 : false;
