7203f42af79e7a018079f7fccabef40c531b9702
Federico Brigante <bfred-it@users.noreply.github.com>
2017-05-09 10:07:22 +0800

    1.0.5

--- dist/supports-webp.js
+++ dist/supports-webp.js
@@ -1,10 +1,10 @@
-/*! npm.im/supports-webp 1.0.4 */
+/*! npm.im/supports-webp 1.0.5 */
 var supportsWebP = (function () {
 'use strict';
 
-var canvas = document.createElement('canvas');
+var canvas = typeof document === 'object' ? document.createElement('canvas') : {};
 canvas.width = canvas.height = 1;
-var index = canvas.toDataURL && canvas.toDataURL('image/webp').indexOf('image/webp') === 5;
+var index = canvas.toDataURL ? canvas.toDataURL('image/webp').indexOf('image/webp') === 5 : false;
 
 return index;
 
