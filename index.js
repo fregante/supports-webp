372ae91ea8bf649f522f4a49ac3d51cc30dbf5e1
Federico Brigante <me@fregante.com>
2022-09-10 14:07:30 +0700

    Meta updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default new Promise(resolve => {
 	const image = new Image();
-	image.onerror = () => resolve(false);
-	image.onload = () => resolve(image.width === 1);
+	image.addEventListener('error', () => resolve(false));
+	image.addEventListener('load', () => resolve(image.width === 1));
 	image.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
 }).catch(() => false);
